607c1da9a669c60e15deb013bdff528bf4f72484
yvesp78 <yves.p78@gmail.com>
2025-09-06 09:54:03 +0000

    first version

--- app/main.py
+++ app/main.py
@@ -3,4 +3,4 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.get("/")
 def read_root():
-    return {"Hello": "We Love Datascientest !!"}
+    return {"Hello": "We Love Datascientest, and we did it. We build a CI/CD Pipeline !!"}
